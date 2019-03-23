d81e0080f137b54f26c7479cff7b85c89383055d
BachPhan <bachp2@uw.edu>
2019-03-22 21:35:12 -0700

    delete grid shader

--- FEASE/FEASE/main.cpp
+++ FEASE/FEASE/main.cpp
@@ -111,7 +111,6 @@ int main(int, char**)
 	texShader.setInt("texture1", 0);
 	
 
-
 	// Render Loop
 	// -----------
 	while (!glfwWindowShouldClose(window))
@@ -571,65 +570,3 @@ inline static GLFWwindow* initApp() {
 	ImGui::GetStyle().WindowRounding = 2.0f;// <- Set this on init or use ImGui::PushStyleVar()
 	return window;
 }
-
-//#version 330 core
-//uniform int divisions;
-//uniform lowp float thickness;
-//uniform vec3 gridColor;
-//
-//in vec2 vUV;
-//
-//void main() {
-	//// multiplicationFactor scales the number of stripes
-	//vec2 t = vUV *divisions;
-
-	//// the threshold constant defines the with of the lines
-	//if (abs(t.x - round(t.x)) <= thickness || abs(t.y - round(t.y)) < thickness)
-	//	gl_FragColor = vec4(gridColor, 1.0);
-	//else
-	//	discard;
-//}
-
-//float x = fract(vUV.x * divisions);
-//x = min(x, 1.0 - x);
-//
-//float xdelta = fwidth(x);
-//x = smoothstep(x - xdelta, x + xdelta, thickness);
-//
-//float y = fract(vUV.y * divisions);
-//y = min(y, 1.0 - y);
-//
-//float ydelta = fwidth(y);
-//y = smoothstep(y - ydelta, y + ydelta, thickness);
-//
-//float r = clamp(x + y, backgroundColor.x, gridColor.x);
-//float g = clamp(x + y, backgroundColor.y, gridColor.y);
-//float b = clamp(x + y, backgroundColor.z, gridColor.z);
-//gl_FragColor = vec4(r, g, b, 1.0);
-
-//#version 330 core
-//#extension GL_OES_standard_derivatives : enable
-//
-//precision highp float;
-//
-//uniform int multiplicationFactor;
-//uniform float threshold;
-//uniform vec4 gridColor;
-//uniform vec2 resolution;  // width and height of the viewport
-//						  //uniform float minthres = 0.000002;
-//						  //uniform float maxthres = 0.00005;
-//in vec2 vUV;
-//
-//void main() {
-//	//float m   = float(multiplicationFactor);
-//	vec2 t = vUV * multiplicationFactor;
-//	vec2 dist = abs(fract(t - 0.5) - 0.5) / resolution;
-//	vec2 th = threshold / resolution;
-//
-//	//th = clamp(th, minthres, maxthres);
-//
-//	if (dist.x > th.x  && dist.y > th.y)
-//		discard;
-//	//gl_FragColor = vec4(vec3(1.0 - min(line, 1.0)), 1.0);
-//	gl_FragColor = gridColor;
-//}
