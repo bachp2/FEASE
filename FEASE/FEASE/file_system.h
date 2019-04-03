bdedfcf59761530dd8b045221e55105f38c9859a
BachPhan <bachp2@uw.edu>
2019-04-02 23:00:37 -0700

    add partial path to the project root-- removed hand coded path

--- FEASE/FEASE/file_system.h
+++ FEASE/FEASE/file_system.h
@@ -3,13 +3,19 @@
 #include <sys/stat.h>
 #include <sstream>
 #include <vector>
+#include <conio.h>
+#include <stdlib.h>
+#include <direct.h>
 
 #define FPATH(X) pathTo(#X).c_str()
 
 //work only for Windows system
+std::string GetFulProjectlPath(const char * partialPath);
 static const std::string pathTo(std::string path)
 {
-	const std::string root = "C:\\Users\\bachp2\\Documents\\IFEM\\FEASE\\FEASE";
+	//const std::string root = "C:\\Users\\bachp2\\Documents\\IFEM\\FEASE\\FEASE";
+	std::string root = GetFulProjectlPath( "..\\" );
+	//root = "C:\\Users\\bachp2\\Documents\\IFEM\\FEASE\\FEASE\\";
 
 	std::vector<std::string> vect;
 	std::stringstream ss(path);
@@ -21,14 +27,30 @@ static const std::string pathTo(std::string path)
 		vect.push_back(substr);
 			
 	}
+
 	auto new_path = root;
 	for (const auto i : vect)
 	{
-		new_path.append("\\");
 		new_path.append(i);
+		new_path.append("\\");
 	}
+	new_path.pop_back(); // remove last \
+
 	return new_path;
 }
+
+inline static std::string GetFulProjectlPath( const char * partialPath )
+{
+	char full[_MAX_PATH];
+	if( _fullpath( full, partialPath, _MAX_PATH ) != NULL )
+	{
+		printf( "Full path is: %s\n", full );
+		return full;
+	}
+	printf( "Invalid path\n" );
+	return nullptr;
+}
+
 
 inline bool checkIfFileExist(const char *name) {
 	struct stat buffer;
