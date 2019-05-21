34c7a5f05d6e365d9fb1fb4aefa92d00d9cdd10c
BachPhan <bachp2@uw.edu>
2019-05-21 00:29:36 -0700

    nearrest filter to allow for scriper text

--- Project/sources/text_render.cpp
+++ Project/sources/text_render.cpp
@@ -35,7 +35,10 @@ void TextPainter::_initfont()
 	glBindTexture(GL_TEXTURE_2D, font.texture);
 	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, font.atlasWidth, font.atlasHeight, 0, GL_RED, GL_UNSIGNED_BYTE, temp_bitmap);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	/*glHint(GL_GENERATE_MIPMAP_HINT, GL_FASTEST);
 	glGenerateMipmap(GL_TEXTURE_2D);*/
 
