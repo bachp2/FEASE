b49640e69e8a323a8fdb6e48f592ee164d868927
BachPhan <bachp2@uw.edu>
2019-07-03 20:04:50 -0700

    error enum

--- Project/sources/error.h
+++ Project/sources/error.h
@@ -6,6 +6,29 @@
 inline void print_opengl_error() {
 	GLenum error = GL_NO_ERROR;
 	while ((error = glGetError()) != GL_NO_ERROR) {
-		printf("OpenGL error: %d", error);
+		switch(error)
+		{
+			case GL_INVALID_ENUM:
+				printf("OpenGL error: %s\n", "GL_INVALID_ENUM");
+				break;
+			case GL_INVALID_VALUE:
+				printf("OpenGL error: %s\n", "GL_INVALID_VALUE");
+				break;
+			case GL_INVALID_OPERATION:
+				printf("OpenGL error: %s\n", "GL_INVALID_OPERATION");
+				break;
+			case GL_STACK_OVERFLOW:
+				printf("OpenGL error: %s\n", "GL_STACK_OVERFLOW");
+				break;
+			case GL_STACK_UNDERFLOW:
+				printf("OpenGL error: %s\n", "GL_STACK_UNDERFLOW");
+				break;
+			case GL_OUT_OF_MEMORY:
+				printf("OpenGL error: %s\n", "GL_OUT_OF_MEMORY");
+				break;
+			case GL_INVALID_FRAMEBUFFER_OPERATION:
+				printf("OpenGL error: %s\n", "GL_INVALID_FRAMEBUFFER_OPERATION");
+				break;
+		}
 	}
 }
